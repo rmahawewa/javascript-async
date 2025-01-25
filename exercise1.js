1ab2c52f833019b8a46dd39761031f3a9a24b2d5
rmahawewa <rmahawewa@yahoo.com>
2025-01-25 09:50:51 +0530

    write js function that makes HTTP GET request and returns a Promise that solves with the response data

--- exercise1.js
+++ exercise1.js
@@ -13,3 +13,34 @@ function display_message(){
 }
 
 invokeAfterDelay(display_message);
+
+
+/**
+ * http get with promises
+ * 
+ * 
+ * Write a javascript function that makes an HTTP GET request and returns a Promise that resolves with the response data
+ * 
+ */
+
+function make_Get_Request(url){
+    return new Promise((resolve, reject) => {
+        fetch(url)
+        .then(response => {
+            if(!response.ok){
+                throw new Error (`HTTP error!vStatus: ${response.status}`);
+            }
+            return response.json();
+        })
+        .then(data => resilve(data))
+        .catch(error => reject(error))
+    })
+}
+
+make_Get_Request('https://example.com/data')
+    .then(data => {
+        console.log('Response data: ' + data);
+    })
+    .catch(error => {
+        console.log('Error: ' + error.message);
+    });
